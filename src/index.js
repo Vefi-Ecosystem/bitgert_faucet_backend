2ac3bae08088ac3e30f13b4f45f7203958cd8b90
Kingsley Victor <javaprodigy56@gmail.com>
2022-08-07 07:53:15 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -67,6 +67,12 @@ app.use(
 );
 app.use(require('morgan')('combined'));
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.use((req, res, next) => {
   const ip = requestIp.getClientIp(req);
   req.ip = ip;
   next();
